157570cdadb93f063706ea4368ee0c6cc6fb5e8c
Eduardo Varella <evarella@gmail.com>
2017-06-20 20:59:04 -0300

    Versão inicial em php

---
+++ main.php
@@ -0,0 +1,24 @@
+<?php
+$inputFile = fopen('/data/in/tables/source.csv','r');
+$outputFile = fopen('/data/out/tables/result.csv', 'w');
+
+$lineNumber = 0;
+while ($line = fgets($inputFile)) {
+    
+    $newLine = "";
+    if($lineNumber == 0){
+        $newLine = str_replace("\r\n", ",double_number\r\n", $line);
+    }
+    else {
+        $columns = explode(",", $line);
+
+        $newLine = $columns[0] . "," .  str_replace("\r\n", "", $columns[1]) . "," . $columns[0]*2 . "\r\n";
+    }
+    
+    fwrite($outputFile, $newLine);
+    $lineNumber++;
+}
+
+fclose($outputFile);
+fclose($inputFile);
+?>
